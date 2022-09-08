77513c6e5438c4ca2158a43b353b33ef611864af
yes <fryrbeadcider@gmail.com>
2022-09-07 20:17:47 -0400

    yes

---
+++ src/main/java/me/rownox/rowcore/Commands/flyCMD.java
@@ -0,0 +1,13 @@
+package me.rownox.rowcore.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class flyCMD implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+
+    }
+}

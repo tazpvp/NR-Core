224f63627c869b785d313495f61d61089376889b
rownox <rownox.ik@gmail.com>
2022-09-11 09:10:44 -0400

    finally final

--- src/main/java/me/rownox/rowcore/utils/PlayerUtils.java
+++ src/main/java/me/rownox/rowcore/utils/PlayerUtils.java
@@ -1,32 +1,35 @@
-package me.rownox.rowcore.utils;
+final package me.rownox.rowcore.utils;
 
-import me.rownox.rowcore.RowCore;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.entity.Player;
+final import me.rownox.rowcore.RowCore;
+final import org.bukkit.attribute.Attribute;
+final import org.bukkit.entity.Player;
+
+import javax.annotation.Nullable;
 
 import static org.bukkit.ChatColor.translateAlternateColorCodes;
 
-public final class PlayerUtils {
+final public final class PlayerUtils {
 
-    /**
-     * Check if the player has the given permission or the absolute one.
-     * @param p The targeted player
-     * @param permission The permission you want to check for
-     */
-    public static void checkPerms(final Player p, final String permission) {
-        if (!p.hasPermission(permission) || !p.hasPermission("rowcore.*")) return;
+    final /**
+     final * Check if the player has the given permission or the absolute one.
+     final * @param p The targeted player
+     final * @param permission The permission you want to check for
+     final */
+    final public static void checkPerms(final Player p, @Nullable final String permission) {
 
-        p.sendMessage(translateAlternateColorCodes('&', RowCore.getInstance().config.getString("No-Permission") + permission));
-    }
+        final if (!p.hasPermission(permission != null ? permission : "rowcore.*") || !p.hasPermission("rowcore.*")) return;
 
-    /**
-     * Heal a player and set their hunger to full.
-     * @param p The targeted player
-     */
-    public static void healPlr(final Player p) {
+        final p.sendMessage(translateAlternateColorCodes('&', RowCore.getInstance().config.getString("No-Permission") + permission));
+        final }
+
+    final /**
+     final  * Heal a player and set their hunger to full.
+     final  * @param p The targeted player
+     final  */
+    final public static void healPlr(final Player p) {
         final double maxHealth = p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
 
         p.setHealth(maxHealth);
         p.setFoodLevel(20);
-    }
-}
+        final }
+    final }

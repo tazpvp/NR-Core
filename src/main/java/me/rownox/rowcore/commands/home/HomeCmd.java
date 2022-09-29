1c332f1c6079f2f64f562f692c6fbd18311e9e95
rownox <rownox.ik@gmail.com>
2022-09-28 20:02:57 -0400

    Home Objects 2

--- src/main/java/me/rownox/rowcore/commands/home/HomeCmd.java
+++ src/main/java/me/rownox/rowcore/commands/home/HomeCmd.java
@@ -1,16 +1,34 @@
 package me.rownox.rowcore.commands.home;
 
+import me.rownox.rowcore.RowCore;
+import me.rownox.rowcore.data.Home;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 public class HomeCmd implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        RowCore rowcore = RowCore.getInstance();
+
         if (sender instanceof Player p) {
+            if (args.length >= 1) {
+                String homeName = args[0];
+                Home home = rowcore.homesList.getHome(p.getUniqueId());
+                if (home != null) {
+                    for (String pHome : home.getHomes().keySet()) {
+                        if (pHome.equals(homeName)) { //if p has the home
+                            p.teleport(home.getHomes().get(homeName));
+                        }
+                    }
+                } else {
+                    p.sendMessage("you aint got no homes IDIOT");
+                }
+            }
 
         }
 

d3894c4c81bbd8b17c3e04e3b945d9d57c8f2893
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-12-04 09:03:19 -0500

    more wg events

--- src/main/java/me/rownox/nrcore/events/WorldGuard.java
+++ src/main/java/me/rownox/nrcore/events/WorldGuard.java
@@ -6,13 +6,30 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFormEvent;
+import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockPhysicsEvent;
 
 public class WorldGuard implements Listener {
 
     private final FileConfiguration CONFIG = NRCore.getInstance().config;
 
     @EventHandler
+    public void onBlockStateChange(BlockPhysicsEvent e) {
+        WorldGuardCancel(e, "block-state-change");
+    }
+    @EventHandler
     public void onBlockStateChange(BlockFormEvent e) {
+        WorldGuardCancel(e, "block-state-change");
+    }
+
+    @EventHandler
+    public void onBlockStateChange(BlockIgniteEvent e) {
+        WorldGuardCancel(e, "block-state-change");
+    }
+
+    @EventHandler
+    public void onBlockStateChange(BlockFromToEvent e) {
         WorldGuardCancel(e, "block-state-change");
     }
 

3a6ba1cdf652ddee103682cbd52d7860a01c5b9e
Ntdi <79174456+n-tdi@users.noreply.github.com>
2023-01-17 21:17:52 -0500

    update commmand core to handle tabl completion, `1.0.19`

--- src/main/java/world/ntdi/nrcore/utils/command/CommandCore.java
+++ src/main/java/world/ntdi/nrcore/utils/command/CommandCore.java
@@ -2,32 +2,36 @@ package world.ntdi.nrcore.utils.command;
 
 import lombok.NonNull;
 import lombok.Setter;
-import org.bukkit.command.*;
-import world.ntdi.nrcore.utils.config.ConfigUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.plugin.SimplePluginManager;
+import world.ntdi.nrcore.utils.config.ConfigUtils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
-public abstract class CommandCore extends BukkitCommand implements TabCompleter {
+public abstract class CommandCore extends BukkitCommand {
     private static final Field COMMAND_MAP_FIELD;
     private static final Field KNOWN_COMMANDS_FIELD;
 
     /**
-     * Initializes fields prior to use
+     * Initializes command fields with reflection prior to use
      */
     static {
-        try {
+        try { //Command Map field, private -> public
             Field field = SimplePluginManager.class.getDeclaredField("commandMap");
             field.setAccessible(true);
             COMMAND_MAP_FIELD = field;
 
-            field = SimpleCommandMap.class.getDeclaredField("knownCommands");
+            field = SimpleCommandMap.class.getDeclaredField("knownCommands"); // Known commands field, private -> public
             field.setAccessible(true);
             KNOWN_COMMANDS_FIELD = field;
 
@@ -52,40 +56,52 @@ public abstract class CommandCore extends BukkitCommand implements TabCompleter 
         register();
     }
 
+    /**
+     * Register the command into the known command map for Bukkit
+     */
     @SuppressWarnings("unchecked")
     private final void register() {
         try {
-            final CommandMap commandMap = (CommandMap) COMMAND_MAP_FIELD.get(Bukkit.getPluginManager());
+            final CommandMap commandMap = (CommandMap) COMMAND_MAP_FIELD.get(Bukkit.getPluginManager()); // Get fields
             final HashMap<String, Command> known = (HashMap<String, org.bukkit.command.Command>) KNOWN_COMMANDS_FIELD
-                    .get(commandMap);
+                    .get(commandMap); // Get known commands
 
+            // Remove old labels
             if (commandMap.getCommand(getLabel()) != null) {
                 known.remove(getLabel());
             }
 
+            // Remove old aliases
             getAliases().forEach(alias -> {
                 if (commandMap.getCommand(alias) != null) {
                     known.remove(alias);
                 }
             });
 
+            // Re-register updated information
             commandMap.register(getLabel(), this);
         } catch (IllegalArgumentException | IllegalAccessException e) {
             e.printStackTrace();
         }
     }
 
+    public abstract List<String> tabCompletion(CommandSender sender, String[] args);
+
     /**
-     * Executes the command
+     * Called when the command is sent in minecraft
+     * @param sender Source object which is executing this command
+     * @param commandLabel The alias of the command used
+     * @param args All arguments passed to the command, split via ' '
+     * @return
      */
     @Override
     public final boolean execute(CommandSender sender, String commandLabel, String[] args) {
 
-        if (this.getPermission() != null && !sender.hasPermission(getPermission())) {
+        if (this.getPermission() != null && !sender.hasPermission(getPermission())) { // Checks for perms
             sender.sendMessage(invalidPermissionMessage());
         }
 
-        return executeFunction(sender, args, defaultFunction);
+        return executeFunction(sender, args, defaultFunction); // Handles reflection
     }
 
     public boolean executeCommand(@NonNull final CommandSender sender, @NonNull final String[] args,
@@ -103,6 +119,7 @@ public abstract class CommandCore extends BukkitCommand implements TabCompleter 
      */
     private final boolean executeFunction(@NonNull final CommandSender sender, @NonNull final String[] args,
                                           @NonNull final CommandFunction<? extends CommandSender> function) {
+        tabComplete(sender, getName(), tabCompletion(sender, args).toArray(String[]::new));
         try {
             final Method method = function.getClass().getMethod("execute", CommandSender.class, String[].class);
             method.invoke(function, sender, args);

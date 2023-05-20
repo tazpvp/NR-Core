9c12c2a643e17d1bbc90f67a982c35b14770c53d
Ntdi <79174456+n-tdi@users.noreply.github.com>
2023-05-20 13:02:01 -0400

    EnchantmentBookBuilderfix

--- src/main/java/world/ntdi/nrcore/utils/item/builders/EnchantmentBookBuilder.java
+++ src/main/java/world/ntdi/nrcore/utils/item/builders/EnchantmentBookBuilder.java
@@ -1,5 +1,6 @@
 package world.ntdi.nrcore.utils.item.builders;
 
+import lombok.NonNull;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
@@ -7,24 +8,24 @@ import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 
 import java.util.Map;
 
-public class EnchantmentBookBuilder extends ItemBuilder {
+public class EnchantmentBookBuilder {
+    protected Material material;
     protected Map<Enchantment, Integer> enchantmentIntegerMap;
 
-    private EnchantmentBookBuilder() {
-        super();
+    public EnchantmentBookBuilder() {
         this.material = Material.ENCHANTED_BOOK;
     }
 
-    @Override
+    public EnchantmentBookBuilder enchantment(@NonNull final Enchantment enchantment, final int level) {
+        this.enchantmentIntegerMap.put(enchantment, level);
+        return this;
+    }
+
     public ItemStack build() {
-        final ItemStack stack = super.build();
+        final ItemStack stack = new ItemStack(this.material);
         stack.getEnchantments().keySet().forEach(stack::removeEnchantment);
 
         final EnchantmentStorageMeta meta = (EnchantmentStorageMeta) stack.getItemMeta();
-
-        for (Map.Entry<Enchantment, Integer> enchantmentIntegerEntry : this.enchantmentIntegerMap.entrySet()) {
-
-        }
 
         this.enchantmentIntegerMap.entrySet().forEach(enchantmentIntegerEntry -> {
             meta.addStoredEnchant(enchantmentIntegerEntry.getKey(), enchantmentIntegerEntry.getValue(), true);
@@ -32,6 +33,6 @@ public class EnchantmentBookBuilder extends ItemBuilder {
 
         stack.setItemMeta(meta);
 
-        return super.build();
+        return stack;
     }
 }

b6d179f17e126963a682a376775ee671ec4cb5ad
rownox <rownox.ik@gmail.com>
2023-05-29 13:33:29 -0400

    speed cmd fix

--- src/main/java/world/ntdi/nrcore/commands/speed/SpeedCommand.java
+++ src/main/java/world/ntdi/nrcore/commands/speed/SpeedCommand.java
@@ -1,5 +1,6 @@
 package world.ntdi.nrcore.commands.speed;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import world.ntdi.nrcore.utils.command.simple.Label;
@@ -9,6 +10,7 @@ public class SpeedCommand extends NRCommand {
     public SpeedCommand() {
         super(new Label("speed", "nrcore.speed"));
         setNativeExecutor((sender, args) -> {
+
             if (!sender.hasPermission(getLabel().getPermission())) {
                 sendNoPermission(sender);
                 return true;
@@ -19,17 +21,19 @@ public class SpeedCommand extends NRCommand {
                 return true;
             }
 
-            if (args.length > 0) {
-                final int speed = Integer.parseInt(args[0]);
-                if (speed <= 10) {
-                    float a = (float) (speed / 10);
-                    p.setWalkSpeed(a);
-                    p.sendMessage(ChatColor.GOLD + "Speed: " + ChatColor.RED + speed);
-                } else {
-                    p.sendMessage(ChatColor.RED + "Maximum value: 10");
-                }
-            } else {
+            if (args.length < 1) {
                 p.setWalkSpeed(0.2F);
+                return true;
+            }
+
+            final double speed = Integer.parseInt(args[0]);
+
+            if (speed <= 10) {
+                float a = (float) (speed / 10);
+                p.setWalkSpeed(a);
+                p.sendMessage(ChatColor.GOLD + "Speed: " + ChatColor.RED + speed);
+            } else {
+                p.sendMessage(ChatColor.RED + "Maximum value: 10");
             }
 
             return true;

7d8a76f6f321e4fc42e80892e84ce755e7c2e193
rownox <rownox.ik@gmail.com>
2023-06-07 20:12:16 -0400

    preventing fire spread

--- src/main/java/world/ntdi/nrcore/events/WorldGuard.java
+++ src/main/java/world/ntdi/nrcore/events/WorldGuard.java
@@ -2,10 +2,7 @@ package world.ntdi.nrcore.events;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockFormEvent;
-import org.bukkit.event.block.BlockFromToEvent;
-import org.bukkit.event.block.BlockIgniteEvent;
-import org.bukkit.event.block.BlockPhysicsEvent;
+import org.bukkit.event.block.*;
 import world.ntdi.nrcore.NRCore;
 
 public class WorldGuard implements Listener {
@@ -27,6 +24,13 @@ public class WorldGuard implements Listener {
     @EventHandler
     public void onBlockStateChange(BlockFromToEvent e) {
         NRCore.config.cancelWG(e, "block-state-change");
+    }
+
+    @EventHandler
+    public void onBlockSpread(BlockSpreadEvent event) {
+        if (event.getSource().getType().name().contains("FIRE")) {
+            event.setCancelled(true);
+        }
     }
 
 }                                                               

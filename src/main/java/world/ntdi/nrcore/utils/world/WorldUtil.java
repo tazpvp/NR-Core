ff848b8ed5a63b24e720acc6d441ab2ceffc7f39
ntdi <me@ntdi.world>
2023-06-10 11:24:08 -0400

    fixed whatever the hell rownox was doing

--- src/main/java/world/ntdi/nrcore/utils/world/WorldUtil.java
+++ src/main/java/world/ntdi/nrcore/utils/world/WorldUtil.java
@@ -1,5 +1,6 @@
 package world.ntdi.nrcore.utils.world;
 
+import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
@@ -11,78 +12,36 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class WorldUtil {
-    public static void cloneWorld(String sourceName, final String destinationName) {
-        final World sourceWorld = Bukkit.getWorld(sourceName);
-        if (sourceWorld == null) {
-            throw new IllegalArgumentException("Source world not found.");
-        } else if (Bukkit.getWorld(destinationName) != null) {
-            throw new IllegalArgumentException("Destination world already exists. Please choose a different name.");
-        } else {
-            (new BukkitRunnable() {
-                public void run() {
-                    try {
-                        WorldCreator worldCreator = new WorldCreator(destinationName);
-                        World clonedWorld = worldCreator.createWorld();
-
-                        File sourceFolder = sourceWorld.getWorldFolder();
-                        File destinationFolder = clonedWorld.getWorldFolder();
-
-                        WorldUtil.copyFolder(sourceFolder, destinationFolder);
-                    } catch (Exception e) {
-                        throw new RuntimeException("Failed to clone the world: " + e.getMessage());
-                    }
-                }
-            }).runTaskAsynchronously(NRCore.getInstance());
+    public World cloneWorld(String backup, String target) {
+        File srcDir = new File(Bukkit.getServer().getWorldContainer(), backup);
+        if (!srcDir.exists()) {
+            Bukkit.getLogger().warning("World does not exist!");
+            return null;
         }
+        String dee = target;
+        File destDir = new File(Bukkit.getServer().getWorldContainer(), dee);
+        try {
+            FileUtils.copyDirectory(srcDir, destDir);
+            for (File file : destDir.listFiles())
+                if (file.isFile())
+                    if (file.getName().equalsIgnoreCase("uid.dat"))
+                        file.delete();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        Bukkit.getServer().createWorld(new WorldCreator(dee));
+        return Bukkit.getServer().getWorld(dee);
     }
 
-    public static void deleteWorld(String worldName) {
-        World world = Bukkit.getWorld(worldName);
-        if (world == null) {
-            throw new IllegalArgumentException("World not found.");
+    public void deleteWorld(String worldName) {
+        World w = Bukkit.getWorld(worldName);
+        File srcDir = new File(Bukkit.getServer().getWorldContainer(), worldName);
+
+        Bukkit.unloadWorld(w, false);
+        try {
+            FileUtils.deleteDirectory(srcDir);
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-
-        if (world != Bukkit.getWorlds().get(0)) {
-            Bukkit.unloadWorld(world, false);
-        }
-
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                File worldFolder = world.getWorldFolder();
-                deleteFolder(worldFolder);
-            }
-        }.runTaskAsynchronously(NRCore.getInstance());
-    }
-
-    private static void copyFolder(File source, File destination) throws IOException {
-        if (source.isDirectory()) {
-            if (!destination.exists()) {
-                destination.mkdirs();
-            }
-
-            String[] files = source.list();
-            if (files != null) {
-                for (String file : files) {
-                    File srcFile = new File(source, file);
-                    File destFile = new File(destination, file);
-                    copyFolder(srcFile, destFile);
-                }
-            }
-        } else {
-            Files.copy(source.toPath(), destination.toPath());
-        }
-    }
-
-    private static void deleteFolder(File folder) {
-        if (folder.isDirectory()) {
-            File[] files = folder.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    deleteFolder(file);
-                }
-            }
-        }
-        folder.delete();
     }
 }

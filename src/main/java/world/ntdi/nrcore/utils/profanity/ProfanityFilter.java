793126d19807f90456efca150cfe280ca8e1058e
n-tdi <79174456+n-tdi@users.noreply.github.com>
2023-06-18 23:54:01 +0200

    changes to profanity filter

--- src/main/java/world/ntdi/nrcore/utils/profanity/ProfanityFilter.java
+++ src/main/java/world/ntdi/nrcore/utils/profanity/ProfanityFilter.java
@@ -1,189 +1,101 @@
 package world.ntdi.nrcore.utils.profanity;
 
 import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfanityFilter {
-        private TreeNode root;
-        private int badWordStart;
-        private int badWordEnd;
-        private boolean isSuspicionFound;
-        private boolean[] asteriskMark;
+    private static Map<String, String[]> words = new HashMap<>();
 
-        public static void main(String[] args) {
-            ProfanityFilter badWordsFilterHash = new ProfanityFilter();
-            badWordsFilterHash.start(args);
-        }
+    private static int largestWordLength = 0;
 
-	public ProfanityFilter() {
-            root = new TreeNode();
-        }
-
-        /**
-         * Ask user to input something, then display the filtered text
-         *
-         * @param args
-         */
-        public void start(String[] args) {
-            buildDictionaryTree(args[0]); // bad words list file name
-            String userInput = readUserInput();
-            // show filtered bad words
-            String filteredBadWords = filterBadWords(userInput);
-            System.out.println(filteredBadWords);
-        }
-
-        /**
-         * Setup a tree for profanity filter
-         *
-         * @param fileName
-         */
-        public void buildDictionaryTree(String fileName) {
-            String line;
-            BufferedReader in = null;
-            try {
-                in = new BufferedReader(new FileReader(fileName));
-                while ((line = in.readLine()) != null) {
-                    // for each bad word
-                    addToTree(line, 0, root);
-                }
-
-            } catch (FileNotFoundException e) { // FileReader
-                e.printStackTrace();
-            } catch (IOException e) { // readLine
-                e.printStackTrace();
-            } finally {
+    public static void loadConfigs() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new URL("https://docs.google.com/spreadsheets/d/1hIEi2YG3ydav1E06Bzf2mQbGZ12kh2fe4ISgLg_UBuM/export?format=csv").openConnection().getInputStream()));
+            String line = "";
+            int counter = 0;
+            while((line = reader.readLine()) != null) {
+                counter++;
+                String[] content = null;
                 try {
-                    in.close();
-                } catch (IOException e) {
+                    content = line.split(",");
+                    if(content.length == 0) {
+                        continue;
+                    }
+                    String word = content[0];
+                    String[] ignore_in_combination_with_words = new String[]{};
+                    if(content.length > 1) {
+                        ignore_in_combination_with_words = content[1].split("_");
+                    }
+
+                    if(word.length() > largestWordLength) {
+                        largestWordLength = word.length();
+                    }
+                    words.put(word.replaceAll(" ", ""), ignore_in_combination_with_words);
+
+                } catch(Exception e) {
                     e.printStackTrace();
                 }
+
             }
+//            System.out.println("Loaded " + counter + " words to filter out");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        /**
-         * @param badWordLine
-         * @param characterIndex
-         *            : index of each letter in a bad word
-         * @param node
-         *            that iterates through the tree
-         */
-        private void addToTree(String badWordLine, int characterIndex, TreeNode node) {
-            if (characterIndex < badWordLine.length()) {
-                Character c = badWordLine.charAt(characterIndex);
-                if (!node.containsChild(c)) {
-                    node.addChild(c);
-                }
-                node = node.getChildByLetter(c);
-                // check if this is the last letter
-                if (characterIndex == (badWordLine.length() - 1)) {
-                    // mark this letter as the end of a bad word
-                    node.setEnd(true);
-                } else {
-                    // add next letter
-                    addToTree(badWordLine, characterIndex + 1, node);
-                }
-            }
+    }
+
+
+    /**
+     * Iterates over a String input and checks whether a cuss word was found in a list, then checks if the word should be ignored (e.g. bass contains the word *ss).
+     * @param input
+     * @return
+     */
+    public static ArrayList<String> badWordsFound(String input) {
+        if(input == null) {
+            return new ArrayList<>();
         }
 
-        /**
-         * Ask the user to input something
-         *
-         * @return string that user inputs
-         */
-        private String readUserInput() {
-            String userInput = null;
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Say something: ");
-            try {
-                userInput = br.readLine();
-            } catch (IOException ioe) {
-                System.out.println("readLine error!!!");
-                System.exit(1);
-            } finally {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return userInput;
-        }
+        // don't forget to remove leetspeak, probably want to move this to its own function and use regex if you want to use this
 
-        /**
-         * @param userInput
-         * @return string with bad words filtered
-         */
-        public String filterBadWords(String userInput) {
-            init(userInput.length());
-            // for each character in a bad word
-            for (int i = 0; i < userInput.length(); i++) {
-                searchAlongTree(userInput, i, root);
-            }
-            return applyAsteriskMark(userInput);
-        }
+        input = input.replaceAll("1","i");
+        input = input.replaceAll("!","i");
+        input = input.replaceAll("3","e");
+        input = input.replaceAll("4","a");
+        input = input.replaceAll("@","a");
+        input = input.replaceAll("5","s");
+        input = input.replaceAll("7","t");
+        input = input.replaceAll("0","o");
+        input = input.replaceAll("9","g");
 
-        private void init(int length) {
-            asteriskMark = new boolean[length];
-            for (int i = 0; i < length; i++) {
-                asteriskMark[i] = false;
-            }
-            badWordStart = -1;
-            badWordEnd = -1;
-            isSuspicionFound = false;
-        }
 
-        private void searchAlongTree(String pUserInput, int characterIndex,
-        TreeNode node) {
-            if (characterIndex < pUserInput.length()) {
-                // get the corresponding letter
-                Character letter = pUserInput.charAt(characterIndex);
-                if (node.containsChild(letter)) {
-                    // find a word whose first letter is equal to one of the bad
-                    // words' first letter
-                    if (isSuspicionFound == false) {
-                        isSuspicionFound = true;
-                        badWordStart = characterIndex;
+        ArrayList<String> badWords = new ArrayList<>();
+        input = input.toLowerCase().replaceAll("[^a-zA-Z]", "");
+
+        // iterate over each letter in the word
+        for(int start = 0; start < input.length(); start++) {
+            // from each letter, keep going to find bad words until either the end of the sentence is reached, or the max word length is reached.
+            for(int offset = 1; offset < (input.length()+1 - start) && offset < largestWordLength; offset++)  {
+                String wordToCheck = input.substring(start, start + offset);
+                if(words.containsKey(wordToCheck)) {
+                    // for example, if you want to say the word bass, that should be possible.
+                    String[] ignoreCheck = words.get(wordToCheck);
+                    boolean ignore = false;
+                    for(int s = 0; s < ignoreCheck.length; s++ ) {
+                        if(input.contains(ignoreCheck[s])) {
+                            ignore = true;
+                            break;
+                        }
                     }
-                    // if this is the final letter of a bad word
-                    if (node.getChildByLetter(letter).isEnd()) {
-                        badWordEnd = characterIndex;
-                        markAsterisk(badWordStart, badWordEnd);
+                    if(!ignore) {
+                        badWords.add(wordToCheck);
                     }
-                    node = node.getChildByLetter(letter);
-                    searchAlongTree(pUserInput, characterIndex + 1, node);
-                } else {
-                    // initialize some parameters
-                    isSuspicionFound = false;
-                    badWordStart = -1;
-                    badWordEnd = -1;
                 }
             }
         }
 
-        /**
-         * Replace some of the letters in userInput as * according to asteriskMark
-         *
-         * @param userInput
-         * @return string with bad words filtered
-         */
-        private String applyAsteriskMark(String userInput) {
-            StringBuilder filteredBadWords = new StringBuilder(userInput);
-            for (int i = 0; i < asteriskMark.length; i++) {
-                if (asteriskMark[i] == true) {
-                    filteredBadWords.setCharAt(i, '*');
-                }
-            }
-            return filteredBadWords.toString();
-        }
-
-        /**
-         * Identify the letters of userInput that should be marked as "*"
-         *
-         * @param badWordStart
-         * @param badWordEnd
-         */
-        private void markAsterisk(int badWordStart, int badWordEnd) {
-            for (int i = badWordStart; i <= badWordEnd; i++) {
-                asteriskMark[i] = true;
-            }
-        }
+        return badWords;
+    }
 }

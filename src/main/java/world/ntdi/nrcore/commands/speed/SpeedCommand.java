202f611266742f6e1973de722d389a4c6894f11c
rownox <rownox.ik@gmail.com>
2023-09-17 20:55:09 -0400

    speed

--- src/main/java/world/ntdi/nrcore/commands/speed/SpeedCommand.java
+++ src/main/java/world/ntdi/nrcore/commands/speed/SpeedCommand.java
@@ -27,6 +27,7 @@ public class SpeedCommand extends NRCommand {
 
         if (args.length < 1) {
             p.setWalkSpeed(0.2F);
+            p.setFlySpeed(0.2F);
             return true;
         }
 
@@ -35,6 +36,7 @@ public class SpeedCommand extends NRCommand {
         if (speed <= 10) {
             float a = (float) (speed / 10);
             p.setWalkSpeed(a);
+            p.setFlySpeed(a);
             p.sendMessage(ChatColor.GOLD + "Speed: " + ChatColor.RED + speed);
         } else {
             p.sendMessage(ChatColor.RED + "Maximum value: 10");
